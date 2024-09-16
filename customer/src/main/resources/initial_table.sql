0a55a09742b73dd4abe9c1cb4585001e638e682b
Arthur Raposo <araposo@foro.io>
2024-09-16 03:07:21 -0300

    fix: github actions

--- customer/src/main/resources/initial_table.sql
+++ customer/src/main/resources/initial_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE customers (
                            id SERIAL PRIMARY KEY,
                            name VARCHAR(100) NOT NULL,
                            surname VARCHAR(100) NOT NULL,
-                           customer_id UUID DEFAULT gen_random_uuid() NOT NULL,  -- Unique identifier for the customer
+                           customer_id UUID NOT NULL,  -- Unique identifier for the customer
                            created_by VARCHAR(100),  -- Reference to the user who created the customer
                            updated_by VARCHAR(100),  -- Reference to the user who last updated the customer
                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Timestamp of creation
